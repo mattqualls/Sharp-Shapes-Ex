33abb030f07fbcb3a2d3b0c6662cd5141506c739
Matt Qualls <mattqualls@comcast.net>
2016-04-21 14:41:24 -0500

    added initial shape class

---
+++ SharpShapesExercise/SharpShapesExercise/Shape.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpShapesExercise
+{
+    public class Shape
+    {
+        public double length { get; set; }
+        public double width { get; set; }
+        public double height { get; set; }
+        public double depth { get; set; }
+        public double radius { get; set; }
+        public int sides { get; set; }
+
+        public virtual double calculateArea()
+        {
+            return width * height;
+        }
+
+        public virtual double calculateVolume()
+        {
+            return width * height * depth;
+        }
+    }
+}
