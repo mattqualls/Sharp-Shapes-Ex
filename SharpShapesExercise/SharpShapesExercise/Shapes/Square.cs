cf3a3eaea4d5fe6172991e8af7aae91da88eeb2b
Matt Qualls <mattqualls@comcast.net>
2016-04-21 15:06:35 -0500

    added square class properties

--- SharpShapesExercise/SharpShapesExercise/Shapes/Square.cs
+++ SharpShapesExercise/SharpShapesExercise/Shapes/Square.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace SharpShapesExercise.Shapes
 {
-    class Square
+    public class Square : Shape
     {
+        public Square()
+        {
+            this.sides = 4;
+        }
+        public override double calculateArea()
+        {
+            return width * width;
+        }
     }
 }
