1d04995871d1d3458887fa9bacaff23eaa2b4382
Matt Qualls <mattqualls@comcast.net>
2016-04-21 15:16:30 -0500

    console menu select written in progam.cs

--- SharpShapesExercise/SharpShapesExercise/Program.cs
+++ SharpShapesExercise/SharpShapesExercise/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace SharpShapesExercise
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            int shapeChoice;
+            double dimension;
+            string[] dimensions;
+            bool shapeSelected = false;
+
+
+            //Console select menu
+            Console.WriteLine("");
+            Console.WriteLine("Select a shape:");
+            Console.WriteLine("1. Circle");
+            Console.WriteLine("2. Square");
+            Console.WriteLine("3. Cylinder");
+            Console.WriteLine("4. Pyramid");
+            Console.WriteLine("> ");
+
+
         }
     }
 }
